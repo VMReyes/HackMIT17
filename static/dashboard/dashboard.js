548bd42a9eeb5561ed8e61a6c14e744f7de49b32
Victor Reyes <vmreyes@mit.edu>
2017-09-17 02:25:04 -0400

    wtf

--- static/dashboard/dashboard.js
+++ static/dashboard/dashboard.js
@@ -1,53 +1,54 @@
-function bac_evaluation(){
+
+
+function replaceButtons(){
   if (sex = 'female') {
     r = 0.55;
   } else {
     r = 0.68;
   }
   current_mass += volume * concentration * 0.789;
-  console.log(current_mass);
   bac = (current_mass / (weight * r)) - (0.015 * hours);
   $('#BAC').text(Number((bac).toFixed(2))+'%');
-}
-
-function hr_evaluation(){
   hours = bac / 0.015 - current_mass / 0.015 / weight / r - current_hours;
   if (hours >= 0.0167) {
     //goto timer
   }
-}
 
-function resetButtons(){
-  $('#oneoz').text("1.5oz").on('click', function(){volume=1.5;replaceButtons()});
-  $('#twooz').text("5.0oz").on('click', function(){volume=5.0;replaceButtons()});
-  $('#threeoz').text("8.0oz").on('click', function(){volume=8.0;replaceButtons()});
-  $('#fouroz').text("12.0oz").on('click', function(){volume=12.0;replaceButtons()});
+  $('#Buttonsb_top').fadeOut().css('display','none');
+  $('#Buttonsb_bot').fadeOut().css('display','none');
+  $('#Buttons_bot').fadeIn();
+  $('#Buttons_top').fadeIn();
 }
 
 
-function replaceButtons(){
-  $('#oneoz').text('5%').on('click', function(){concentration=.05;bac_evaluation();volume=0;concentration=0;hr_evaluation();resetButtons()});
-  $('#twooz').text('10%').on('click', function(){concentration=.10;bac_evaluation();volume=0;concentration=0;hr_evaluation();resetButtons()});
-  $('#threeoz').text('30%').on('click', function(){concentration=.30;bac_evaluation();volume=0;concentration=0;hr_evaluation();resetButtons()});
-  $('#fouroz').text('40%').on('click', function(){concentration=.40;bac_evaluation();volume=0;concentration=0;hr_evaluation();resetButtons()});
-}
 function setupHandlers() {
-  $('#oneoz').on('click', function(){volume=1.5;replaceButtons()});
-  $('#twooz').on('click', function(){volume=5.0;replaceButtons()});
-  $('#threeoz').on('click', function(){volume=8.0;replaceButtons()});
-  $('#fouroz').on('click', function(){volume=12.0;replaceButtons()});
+  $('#oneoz').on('click', function(){volume=1.5;switchButtons()});
+  $('#twooz').on('click', function(){volume=5.0;switchButtons()});
+  $('#threeoz').on('click', function(){volume=8.0;switchButtons()});
+  $('#fouroz').on('click', function(){volume=12.0;switchButtons()});
+  $('#oneozb').on('click', function(){concentration=.05;replaceButtons()});
+  $('#twoozb').on('click', function(){concentration=.10;replaceButtons()});
+  $('#threeozb').on('click', function(){concentration=.30;replaceButtons()});
+  $('#fourozb').on('click', function(){concentration=.40;replaceButtons()});
+}
+
+function switchButtons(){
+  $('#Buttons_top').fadeOut().css('display','none');
+  $('#Buttons_bot').fadeOut().css('display','none');
+  $('#Buttonsb_bot').fadeIn();
+  $('#Buttonsb_top').fadeIn();
 }
 
 $(document).ready(setupHandlers);
 
 var hours = 0.0;
 var bac = 0.00;
-var current_mass = 0;
-var volume = 0;
-var concentration = 0;
-var added_volume = 0;
-var added_concentration = 0;
-var mass = 0;
-var weight = 130;
+var current_mass = 0.0;
+var volume = 0.0;
+var concentration = 0.0;
+var added_volume = 0.0;
+var added_concentration = 0.0;
+var mass = 0.0;
+var weight = 130.0;
 var sex = 'male';
-var current_hours = 0;
+var current_hours = 0.0;
